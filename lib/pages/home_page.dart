793473cebaafac4f8a467b01c5fd0f0714813432
prakhar8922 <92254361+prakhar8922@users.noreply.github.com>
2022-04-10 14:10:31 +0530

    day-13 card,item widget,listview

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: const Text("welcome to my first Flutter app"),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView.builder(
+          // itemCount: CatalogModel.items.length,
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              // item: CatalogModel.items[index],
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
