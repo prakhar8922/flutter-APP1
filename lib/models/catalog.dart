af57a132ff62244efba3107fc3ae67c612f8f162
prakhar8922 <92254361+prakhar8922@users.noreply.github.com>
2022-04-25 12:30:07 +0530

    add cart functionality

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -3,10 +3,10 @@ import 'dart:convert';
 class CatalogModel {
   static List<Item>? items;
   // get item by ID
-  static Item getById(int id) =>
+  Item getById(int id) =>
       items!.firstWhere((element) => element.id == id, orElse: null);
 
-  static Item getByPosition(int pos) => items![pos];
+  Item getByPosition(int pos) => items![pos];
 }
 
 class Item {
